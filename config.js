c83fe1215361561b3b777f88fb19afcffbcf19fc
anonymoussprocket <anonymoussprocket@gmail.com>
2021-03-10 23:38:53 -0500

    - added conseil-based collection query - added conseil-based creations query - moved config out

---
+++ config.js
@@ -0,0 +1,17 @@
+const mainnet = {
+    objkts: 'KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton',
+    nftLedger: 511,
+    nftMetadataMap: 514,
+    network: 'mainnet',
+    protocol: 'KT1Hkg5qeNhfwpKW4fXvq7HGZB9z2EnmCCA9',
+    swapsPtr: 523,
+    hDAO: 'KT1AFA2mwNUMNd4SsujE1YYp29vd8BZejyKW',
+    curations: 'KT1TybhR7XraG75JFYKSrh7KnxukMBT5dor6',
+    curationsPtr: 519,
+    royaltiesPtr: 522,
+    ledgerPtr2: 515
+}
+
+module.exports = {
+    networkConfig: mainnet
+}
