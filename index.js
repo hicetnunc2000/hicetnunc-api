95b4eb1822943bb94f448307ae8c315cb3bfed89
Göran Sandström <goran.sandstrom@bookbeat.com>
2021-04-14 16:20:00 +0200

    update package.json

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 require('module').Module._initPaths()
 require('dotenv').config()
 
@@ -16,9 +15,10 @@ const app = express()
 app.use(express.json())
 app.use(cors({ origin: '*' }))
 app.use(router)
-/*
-app.listen(PORT, () => {
-  console.log(`SERVER RUNNING ON localhost:${PORT}`)
-})*/
 
+if (process.env.NODE_ENV === 'development') {
+  app.listen(PORT, () => {
+    console.log(`SERVER RUNNING ON localhost:${PORT}`)
+  })
+}
 module.exports.handler = serverless(app)
